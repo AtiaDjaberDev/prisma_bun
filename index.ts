b36d90c69c6c8c6ec662304e8e341916b607ef98
Atia Djaber <atiadjaber39@gmail.com>
2025-01-11 16:10:55 +0100

    fix issues

--- index.ts
+++ index.ts
@@ -1,25 +1,32 @@
 import { PrismaClient } from "@prisma/client";
 import { $ } from "bun";
+// import process from "process";
 
 const prisma = new PrismaClient();
 
-// create a new user
-
-const host = process.argv[2] || "localhost";
-
+const host = process.argv[2] || import.meta.env.HOST;
+ 
 const server = Bun.serve({
   port: 3000,
   hostname: host,
   idleTimeout: 120,
   async fetch(request) {
-   
-    const output = await $`npx prisma migrate dev --skip-generate`;
-    console.log(output.exitCode);
-    const products = await prisma.category.findMany();
+    const url = new URL(request.url);
+    console.log(url.pathname);
 
-    // count the number of users
-    console.log(`There are ${products.length} users in the database.`);
-    return Response.json(products);
+    if (url.pathname === "/test") {
+      const products = await prisma.category.findMany();
+      // count the number of users
+      console.log(`Host ${import.meta.env.HOST}`);
+      return Response.json(products);
+      
+    }else{
+      const output = await $`npx prisma migrate dev --skip-generate`;
+      console.log(output.exitCode);
+  
+      return Response.json(output);
+    }
+
   },
 });
 
