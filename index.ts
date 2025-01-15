6a95ed56a08daf366a0fe92c2c60c526607cc02c
Atia Djaber <atiadjaberdev.com>
2025-01-15 10:28:51 +0100

    fix

--- index.ts
+++ index.ts
@@ -1,7 +1,27 @@
 import { PrismaClient } from "@prisma/client";
 import { $ } from "bun";
 
+
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/mysql2";
+import mysql from "mysql2/promise";
+import "dotenv/config";
+import * as dotenv from "dotenv";
+import { customersTable } from "./src/db/schema";
+dotenv.config();
+
 const prisma = new PrismaClient();
+
+const poolConnection = mysql.createPool({
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "sales",
+});
+
+// إنشاء كائن Drizzle
+const db = drizzle(poolConnection);
+
 
 // create a new user
 
@@ -9,17 +29,16 @@ const host = process.argv[2] || "localhost";
 
 const server = Bun.serve({
   port: 3000,
-  hostname: host,
+  hostname: "localhost",
   idleTimeout: 120,
   async fetch(request) {
-   
-    const output = await $`npx prisma migrate dev --skip-generate`;
-    console.log(output.exitCode);
-    const products = await prisma.category.findMany();
 
-    // count the number of users
-    console.log(`There are ${products.length} users in the database.`);
-    return Response.json(products);
+    // const output = await $`cd .. && npx drizzle-kit push`;
+    // console.log(output.exitCode);
+ 
+    const customers = await db.select().from(customersTable);
+    console.log("All Customers:", customers);
+    return Response.json(customers);
   },
 });
 
---
+++ src/db/schema.ts
@@ -0,0 +1,12 @@
+import { int, mysqlTable, serial, varchar } from "drizzle-orm/mysql-core";
+export const customersTable = mysqlTable("customers", {
+  id: serial().primaryKey(),
+  name: varchar({ length: 255 }).notNull(),
+  age: int().notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+  debt: int().notNull(),
+  test: int(),
+  hello: int(),
+  hello2: int(),
+  ancien: int().notNull(),
+});
